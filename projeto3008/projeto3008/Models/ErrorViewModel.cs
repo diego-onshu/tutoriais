c2ab4ada57a719cf65d70f9e08960ad3b5b66a70
Diego J <diego.honshu@hotmail.com>
2019-08-30 22:26:09 -0300

    Primeiro commit, criando projeto

---
+++ projeto3008/projeto3008/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace projeto3008.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
